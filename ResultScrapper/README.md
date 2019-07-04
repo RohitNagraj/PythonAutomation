b5a0264b740579390749a0f39735bcbf6cf17d46
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-07-04 18:53:25 +0530

    Added explaination

---
+++ ResultScrapper/README.md
@@ -0,0 +1,9 @@
+# MSRIT Result Scrapper
+This script scrapes all results from the exam.msrit.edu website and stores it in a text file.
+
+## Usage
+Change the USNs according to your requirements, run the script. That's it!
+
+## Bugs
+It doesn't handle the USNs that are not present and waits infinitely.
+
--- ResultScrapper/ResultScrapper.py
+++ ResultScrapper/ResultScrapper.py
@@ -1,3 +1,14 @@
+"""
+    @Author: Rohit Nagraj 
+    @Date: 2019-07-04 18:45:11 
+    @Last Modified by:   Rohit Nagraj 
+    @Last Modified time: 2019-07-04 18:45:11 
+"""
+
+# This script scrapes the exam.msrit.edu site and stores all results in a text file
+# It exploits the fact that the site uses same captcha when u come back after viewing a result.
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec
@@ -9,8 +20,10 @@ chrome = webdriver.Chrome('./chromedriver')
 
 wait = WebDriverWait(chrome, 600)
 
+# Website to go to
 chrome.get('http://exam.msrit.edu')
 
+# XPATHs for all the elements we will be interacting with
 x_usn = '//input[@id="usn"]'
 x_captcha = '//input[@id="osolCatchaTxt0"]'
 x_go = '//input[@class="buttongo"]'
@@ -19,9 +32,13 @@ x_USN = '//td[@style=" text-transform:uppercase;"]'
 x_sgpa = '//*[@id="main"]/div/table/tbody/tr[11]/td/table/tbody/tr/td[2]/table/tbody/tr/td[4]/div/span[2]'
 x_cgpa = '//*[@id="main"]/div/table/tbody/tr[11]/td/table/tbody/tr/td[2]/table/tbody/tr/td[5]/div/span[2]'
 
+# File to write the results in
 f = open("Results.txt", "a")
+
+# The range is the USN range
 for i in range(1, 150):
 
+    # Change your USNs for your banch and year accordingly
     usn = wait.until(ec.presence_of_element_located((By.XPATH, x_usn)))
     if (i < 10):
         usn.send_keys("1MS17IS00" + str(i))
@@ -31,6 +48,8 @@ for i in range(1, 150):
         usn.send_keys("1MS17IS" + str(i))
 
     captcha = wait.until(ec.presence_of_element_located((By.XPATH, x_captcha)))
+
+    # Takes captcha as input from user in the first iteration
     if i == 1:
         temp = input()
     captcha.send_keys(temp)
@@ -43,6 +62,7 @@ for i in range(1, 150):
     sgpa = wait.until(ec.presence_of_element_located((By.XPATH, x_sgpa)))
     cgpa = wait.until(ec.presence_of_element_located((By.XPATH, x_cgpa)))
 
+    # Writing results to the file
     f.write(USN.text[6:] + "        ")
     f.write(name.text)
     for i in range(30-len(name.text)):
