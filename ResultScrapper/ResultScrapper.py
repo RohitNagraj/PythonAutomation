1c1ca2da0fb2d5442827fcb37f1b3c396235558d
Rohit Nagraj <rohitnagraj@graphenesvc.com>
2020-08-10 10:29:35 +0530

    Updated for the new results page

--- ResultScrapper/ResultScrapper.py
+++ ResultScrapper/ResultScrapper.py
@@ -18,7 +18,7 @@ import time
 
 chrome = webdriver.Chrome('./chromedriver')
 
-wait = WebDriverWait(chrome, 600)
+wait = WebDriverWait(chrome, 5)
 
 # Website to go to
 chrome.get('http://exam.msrit.edu')
@@ -27,45 +27,48 @@ chrome.get('http://exam.msrit.edu')
 x_usn = '//input[@id="usn"]'
 x_captcha = '//input[@id="osolCatchaTxt0"]'
 x_go = '//input[@class="buttongo"]'
-x_name = '//td[@class="headingdateWhite"]'
-x_USN = '//td[@style=" text-transform:uppercase;"]'
-x_sgpa = '//*[@id="main"]/div/table/tbody/tr[11]/td/table/tbody/tr/td[2]/table/tbody/tr/td[4]/div/span[2]'
-x_cgpa = '//*[@id="main"]/div/table/tbody/tr[11]/td/table/tbody/tr/td[2]/table/tbody/tr/td[5]/div/span[2]'
+x_name = '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/h3'
+x_USN = '/html/body/div[2]/div/div/div[3]/div/div/div[2]/div/h2'
+x_sgpa = '/html/body/div[2]/div/div/div[4]/div/div/div[1]/div/div[3]/div/p'
+x_cgpa = '/html/body/div[2]/div/div/div[4]/div/div/div[1]/div/div[4]/div/p'
 
 # File to write the results in
 f = open("Results.txt", "a")
 
 # The range is the USN range
-for i in range(137, 150):
+for i in range(50, 150):
+    try:
 
-    # Change your USNs for your banch and year accordingly
-    usn = wait.until(ec.presence_of_element_located((By.XPATH, x_usn)))
-    usn.send_keys("1MS17IS" + str(i).zfill(3))
+        # Change your USNs for your banch and year accordingly
+        usn = wait.until(ec.presence_of_element_located((By.XPATH, x_usn)))
+        usn.send_keys("1MS17IS" + str(i).zfill(3))
 
-    captcha = wait.until(ec.presence_of_element_located((By.XPATH, x_captcha)))
+        captcha = wait.until(ec.presence_of_element_located((By.XPATH, x_captcha)))
 
-    # Takes captcha as input from user in the first iteration
-    if i == 137:
-        temp = input("Enter Captcha: ")
-    captcha.send_keys(temp)
+        # Takes captcha as input from user in the first iteration
+        if i == 50:
+            temp = input("Enter Captcha: ")
+        captcha.send_keys(temp)
 
-    go = wait.until(ec.presence_of_element_located((By.XPATH, x_go)))
-    go.click()
+        go = wait.until(ec.presence_of_element_located((By.XPATH, x_go)))
+        go.click()
 
-    USN = wait.until(ec.presence_of_element_located((By.XPATH, x_USN)))
-    name = wait.until(ec.presence_of_element_located((By.XPATH, x_name)))
-    sgpa = wait.until(ec.presence_of_element_located((By.XPATH, x_sgpa)))
-    cgpa = wait.until(ec.presence_of_element_located((By.XPATH, x_cgpa)))
+        USN = wait.until(ec.presence_of_element_located((By.XPATH, x_USN)))
+        name = wait.until(ec.presence_of_element_located((By.XPATH, x_name)))
+        sgpa = wait.until(ec.presence_of_element_located((By.XPATH, x_sgpa)))
+        cgpa = wait.until(ec.presence_of_element_located((By.XPATH, x_cgpa)))
 
-    # Writing results to the file
-    f.write(USN.text[6:] + "        ")
-    f.write(name.text)
-    for i in range(30-len(name.text)):
-        f.write(" ")
-    f.write(sgpa.text)
-    for i in range(10-len(sgpa.text)):
-        f.write(" ")
-    f.write(cgpa.text + "\n")
+        # Writing results to the file
+        f.write(USN.text[6:] + "        ")
+        f.write(name.text)
+        for i in range(30-len(name.text)):
+            f.write(" ")
+        f.write(sgpa.text)
+        for i in range(10-len(sgpa.text)):
+            f.write(" ")
+        f.write(cgpa.text + "\n")
+    except:
+        pass
 
     chrome.back()
 f.close()
